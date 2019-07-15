035f57fba504b8c323d5c6148e0ec0fd32f8a459
cross-ts <ts.kouhei@gmail.com>
2019-07-16 01:52:31 +0900

    Refactor

--- scrape.go
+++ scrape.go
@@ -6,27 +6,40 @@ import (
 	"net/http"
 )
 
-func Scrape() {
-	res, err := http.Get("https://github.com/trending")
+const GITHUB_TRENDING_URL string = "https://github.com/trending"
+const TREND_QUERY string = "html body div main div div.Box div article.Box-row"
+
+func httpRequest() *http.Response {
+	res, err := http.Get(GITHUB_TRENDING_URL)
 	if err != nil {
 		panic(err)
 	}
+	if res.StatusCode != 200 {
+		panic("github.com/trending didn't return code 200.")
+	}
+	return res
+}
+
+func pickTrendsFrom(response *http.Response) *goquery.Selection {
+	doc, err := goquery.NewDocumentFromReader(response.Body)
+	if err != nil {
+		panic(err)
+	}
+	trendSelections := doc.Find(TREND_QUERY)
+	return trendSelections
+}
+
+func hoge(i int, s *goquery.Selection) {
+	repo, _ := s.Find("h1 > a").Attr("href")
+	desc := s.Find("p").Text()
+	fmt.Println(repo)
+	fmt.Println(desc)
+}
+
+func Scrape() {
+	res := httpRequest()
 	defer res.Body.Close()
 
-	if res.StatusCode != 200 {
-		panic("error")
-	}
-
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	// Find the review items
-	doc.Find("html > body > div > main > div > div.Box > div > article.Box-row").Each(func(i int, s *goquery.Selection) {
-		repo, _ := s.Find("h1 > a").Attr("href")
-		desc := s.Find("p").Text()
-		fmt.Println(repo)
-		fmt.Println(desc)
-	})
+	trends := pickTrendsFrom(res)
+	trends.Each(hoge)
 }
